2e44fffd8292a7bf3d3d65f59d9786303109aad9
Github MMI3 <github@mmi-troyes.fr>
2026-01-23 14:36:54 +0000

    feat: ajout endpoint health check pour Render

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/health', name: 'health_check', methods: ['GET'])]
+    public function health(): JsonResponse
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+            'timestamp' => time(),
+        ], Response::HTTP_OK);
+    }
+}
