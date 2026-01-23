065d5bd2f4611e0fe4e3539fa774bfe4602153c2
Github MMI3 <github@mmi-troyes.fr>
2026-01-23 15:29:14 +0000

    fix: rendre la route / compatible avec le health check Render

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,6 +12,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        // For health check compatibility, return simple JSON if requested by Render
+        $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+        if (str_contains($userAgent, 'Render') || isset($_SERVER['HTTP_X_RENDER'])) {
+            return new JsonResponse(['status' => 'ok'], Response::HTTP_OK);
+        }
+        
         return $this->render('home/index.html.twig');
     }
 }
