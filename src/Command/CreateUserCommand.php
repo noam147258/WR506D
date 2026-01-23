0aea75c8092bed3ffc7c3e5597d695ed43ca11b2
Github MMI3 <github@mmi-troyes.fr>
2026-01-23 18:43:30 +0000

    feat: ajout commande pour créer un utilisateur

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-user',
+    description: 'Crée un nouvel utilisateur',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email de l\'utilisateur')
+            ->addArgument('password', InputArgument::REQUIRED, 'Mot de passe')
+            ->addOption('roles', null, InputOption::VALUE_OPTIONAL, 'Rôles (séparés par des virgules)', 'ROLE_USER')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $roles = explode(',', $input->getOption('roles'));
+
+        // Vérifier si l'utilisateur existe déjà
+        $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        if ($existingUser) {
+            $io->error("Un utilisateur avec l'email '$email' existe déjà.");
+            return Command::FAILURE;
+        }
+
+        // Créer l'utilisateur
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles($roles);
+        
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success("Utilisateur créé avec succès : $email");
+        $io->note("Rôles : " . implode(', ', $roles));
+
+        return Command::SUCCESS;
+    }
+}
